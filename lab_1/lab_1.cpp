693481a20690e4b3a46217401b449a09b2624351
Damir <damir.abdulin@tutanota.com>
2022-02-19 11:10:01 +0300

    Correct used modules

--- lab_1/lab_1.cpp
+++ lab_1/lab_1.cpp
@@ -1,11 +1,11 @@
 ﻿#include <winsock2.h>
-#include <iptypes.h>
 #include <iphlpapi.h>
-//#include <windows.h>
 #include <stdio.h>
 
 #define MALLOC(x) HeapAlloc(GetProcessHeap(), 0, (x))
 #define FREE(x) HeapFree(GetProcessHeap(), 0, (x))
+
+#pragma comment(lib, "IPHLPAPI.lib")
 
 void printAdpaterInfo(PIP_ADAPTER_INFO pAdapter);
 
